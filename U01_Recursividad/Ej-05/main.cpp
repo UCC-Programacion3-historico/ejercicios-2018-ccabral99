986409dba2e16d6026f7c825101c604230dc989e
ccabral99 <48757986+ccabral99@users.noreply.github.com>
2020-02-09 12:36:27 -0300

    Update main.cpp

--- U01_Recursividad/Ej-05/main.cpp
+++ U01_Recursividad/Ej-05/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include "digitos.h"
-
+using namespace std;
+int binario (int n)
+{
+    if(n<2)
+    {
+        return n;
+    } else
+        return n%2+(10*binario(n/2));
+}
 int main() {
-    std::cout << "Ejercicio 01/05\n" << std::endl;
-    return 0;
+    int a,b;
+    cout<<"ingrese los numeros "<<endl;
+    cin>>a>>b;
+    cout<<"parte real de la division "<<cociente(a,b)<<endl;
+    cout<<"espaciado ";
+    escribir_espaciado(a);
 }
