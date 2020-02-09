ac58251be6dce09f7a2490cce17e646183dc1962
ccabral99 <48757986+ccabral99@users.noreply.github.com>
2020-02-09 12:36:05 -0300

    Update digitos.cpp

--- U01_Recursividad/Ej-05/digitos.cpp
+++ U01_Recursividad/Ej-05/digitos.cpp
@@ -4,14 +4,31 @@
 using namespace std;
 
 unsigned int cociente(unsigned int a, unsigned int b) {
-    return 0;
+    if(a<b)
+        return 0;
+    else
+    {
+        return 1+cociente(a-b,b);
+    }
 }
 
 
 unsigned int resto(unsigned int a, unsigned int b) {
-    return 0;
+    if(a<b)
+        return a;
+    else
+        return resto(a-b,b);
+
 }
 
 
 void escribir_espaciado(unsigned int num) {
+    if (num <10)
+    {cout<<"    "<<num<<"   ";}
+
+    else {
+        escribir_espaciado(cociente(num,10));
+        std::cout<<resto(num,10)<<" ";
+    }
+
 }
