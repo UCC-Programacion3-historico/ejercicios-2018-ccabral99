816fa9d7f4359216b24414fd063266cbbbc0e21d
ccabral99 <48757986+ccabral99@users.noreply.github.com>
2020-02-09 12:33:21 -0300

    Update main.cpp

--- U01_Recursividad/Ej-02/main.cpp
+++ U01_Recursividad/Ej-02/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include "sumatoria.h"
+using namespace std;
 
 int main() {
-    std::cout << "Ejercicio 01/02\n" << std::endl;
-    return 0;
+    int i;
+    cout<<"ingrese cantidad de elementos del arreglo "<<endl;
+    cin>>i;
+    int a[i];
+    for (int j=0;j<i;j++)
+        cin>>a[j];
+    int s=sumatoria(a,i);
+    cout<<" la sumatoria total es "<<s<<endl;
 }
