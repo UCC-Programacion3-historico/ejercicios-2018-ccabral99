609dfc9985a37fae8f257544fa74d314c70b31ed
ccabral99 <48757986+ccabral99@users.noreply.github.com>
2020-02-09 12:34:08 -0300

    Update ackerman.cpp

--- U01_Recursividad/Ej-03/ackerman.cpp
+++ U01_Recursividad/Ej-03/ackerman.cpp
@@ -2,5 +2,12 @@
 
 
 unsigned int ackerman(unsigned int m, unsigned int n) {
+
+    if (m==0)
+        return n+1;
+    else if( m>0 && n=0)
+        ackerman  (m-1,1);
+    else if(m>0 && n>0)
+        ackerman (m-1, ackerman(m,n-1));
     return 0;
 }
