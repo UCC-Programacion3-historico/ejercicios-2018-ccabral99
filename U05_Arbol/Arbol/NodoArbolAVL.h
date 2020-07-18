7532bc007e40bbff0b2113c6f1db2862a74a6a7a
ccabral99 <cabralcamila1999@gmail.com>
2020-07-18 12:36:26 -0300

    hasta arbol y un poquitito de hash

---
+++ U05_Arbol/Arbol/NodoArbolAVL.h
@@ -0,0 +1,21 @@
+//
+// Created by Camila Cabral on 18/07/2020.
+//
+
+#ifndef NODOARBOL_H
+#define NODOARBOL_H
+
+template<class T>
+class NodoArbolAVL {
+public:
+    T dato;
+    NodoArbol<T> *izq, *der;
+    char altura;
+
+    NodoArbol(T dato) : dato(dato) {
+            der = izq = nullptr;
+            altura = 0;
+    }
+};
+
+#endif //HASHENTRY_H
